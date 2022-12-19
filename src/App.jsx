f82c4f273f85c590177329fe2b5c75327ca3efd6
nicosz <nicosteinjamer@gmail.com>
2022-12-19 16:28:37 -0300

    feat: home general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import './App.css';
+import './index.css';
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import Home from './components/Home/Home';
+
+
+//import Carrusel from './components/Carrusel/Carrusel';
+//<Carrusel />
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className='app' >
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
   );
 }
 
