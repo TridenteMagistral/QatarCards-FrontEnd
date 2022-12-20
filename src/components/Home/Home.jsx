a892b24212b52c31b642221445bb5b342a205e0b
nicosz <nicosteinjamer@gmail.com>
2022-12-19 23:51:33 -0300

    refactor:some navbar changes and home template content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,25 @@ import "./Home.css"
 
 export default function Home() {
     return (
-        <div className="wallpaper-mundial ">
+        <div className="wallpaper-mundial ps-5">
+            <div className="row">
+                <div className="novedades-principales col-12 col-sm-3  ">
 
+                </div>
+
+                <div className="novedades-secundarias col-12 col-sm-3">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="novedades-terciarias col-12 col-sm-3">
+
+                </div>
+
+                <div className="novedades col-12 col-sm-3 ">
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
@@ -8,37 +7,43 @@ import "./Navbar.css"
 
 function BasicExample() {
   return (
-    <Navbar className='fixed-top' variant="dark" expand="lg">
-      <Container>
+    <Navbar collapseOnSelect className='fixed-top ps-3' expand="sm" variant="dark">
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-        <NavDropdown title={<FontAwesomeIcon icon={faGear} />}  id="basic-nav-dropdown">
+      <div className='d-flex '>
+        <NavDropdown className='mx-3 m-auto d-block d-sm-none icon' title={<FontAwesomeIcon icon={faGear} />} id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">
             Another action
           </NavDropdown.Item>
         </NavDropdown>
-
-      </Container>
-    </Navbar >
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />     
+      </div>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      <NavDropdown className='mx-3 m-auto d-none d-sm-block icon' title={<FontAwesomeIcon icon={faGear} />} id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            Another action
+          </NavDropdown.Item>
+        </NavDropdown>
+    </Navbar>
   );
 }
+
 
 export default BasicExample;
