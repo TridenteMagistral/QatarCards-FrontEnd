ebd48f5aee164c676cb507721f29e97dd5d852ef
nicosz <97996084+nicosz@users.noreply.github.com>
2023-01-31 16:28:55 -0300

    Revert "refactor:code component signUp and feat:customHook"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import './index.css';
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 import Home from './components/Home/Home';
 import Login from './components/SignIn/Login';
 import Layout from './components/Layout/Layout';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUp from './components/SignUp/SignUp';
 
+
+
+
+//import Carrusel from './components/Carrusel/Carrusel';
+//<Carrusel />
+
 function App() {
   return (
     <div className='app' >
       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route element={<Layout />}>
-              <Route path="/home" element={<Home />} />
-            </Route>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div >
   );
