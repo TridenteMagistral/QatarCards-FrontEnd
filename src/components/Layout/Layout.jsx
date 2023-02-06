66bb1d8aaa296adcab693563910a459145e8a31e
nicosz <nicosteinjamer@gmail.com>
2023-02-06 03:28:17 -0300

    feat:navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,10 @@
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
 import { Outlet } from "react-router-dom"
-import './layout.css'
 
 export default function Layout() {
     return (
-        <div className="wallpaper-layout">
+        <div className="position-relative">
             <Navbar />
             <Outlet />
             <Footer />
